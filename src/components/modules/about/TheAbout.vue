7647312a1ed27a7cb4ea6de8725db60f280a5a67
fearthelettuce <twigss@gmail.com>
2023-07-09 11:03:47 -0500

    fixed bugs reported by the cutest QA tester there is

--- src/components/modules/about/TheAbout.vue
+++ src/components/modules/about/TheAbout.vue
@@ -6,7 +6,7 @@
         <h4>About this site</h4>
             <p>I built this site to pracice my front-end software development skills.  I wanted to address some of the frustrations I've seen with other plant sites... 95% out of stock, broken product filters, clunky user interface, etc.  I hope you have a great experience in using this site. If not, I hope you let me know how I can improve it!</p>
         <h4>My Plants</h4>
-        <p>My primary interest in with highland Nepenthes, Heliamphora and Cephalotus. My grow space is limited, so my inventory won't be the same as the larger sellers.  However, I primarily sell rooted divisions/cuttings.  Unlike many other sellers, that import tissue culture plants, the plants you will buy from me are established, actively growing, and </p>   
+        <p>My primary interest in with highland Nepenthes, Heliamphora and Cephalotus. My grow space is limited, so my inventory won't be the same as the larger sellers.  However, I primarily sell rooted divisions/cuttings.  Unlike many other sellers, that import tissue culture plants, the plants you will buy from me are generally established, actively growing, divisions that won't take years to grow to a decent size.</p>   
         <h4>Contact</h4>
         <p>For questions, plesae contact me at fearthelettuce@gmail.com.</p>
         <a href="https://www.ebay.com/usr/fearthelettuce#tab2" target="_blank">See my feedback on eBay</a>
