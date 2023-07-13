3cddbb161c451fda59552d723d437f0ec52d18fc
fearthelettuce <twigss@gmail.com>
2023-07-12 22:50:29 -0500

    worked on account home

--- src/components/modules/account/AccountHome.vue
+++ src/components/modules/account/AccountHome.vue
@@ -1,21 +1,35 @@
 <template>
-    <section class="container-fluid d-flex justify-content-center">
-        <p>{{ userStore.getUserProfile?.name.firstName }}</p>
-        <p>{{ userStore.getUserProfile?.name.lastName }}</p>
-        <p>{{ userStore.getUserProfile?.shippingAddress?.address1 }}</p>
+    <section class="container-fluid d-flex flex-row justify-content-center">
+        <aside>
+            <nav>
+                <ul>
+                    <li>Orders</li>
+                    <li>Addresses</li>
+                    <li>Security</li>
+                </ul>
+            </nav>
+        </aside>
+        <main class="d-flex flex-column mx-5">
+            <p>{{ profile?.name }}</p>
+            <p>{{ profile?.shippingAddress }}</p>
+            <p>{{ profile?.contactInformation }}</p>
+        </main>
     </section>
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from '@/components/modules/auth/stores/users'
-
+import { storeToRefs } from 'pinia';
 const userStore = useUserStore()
-
+const {profile} = storeToRefs(userStore)
 </script>
 
 <style scoped>
     section {
         padding: 0 clamp(.1em, 6vw, 20rem);
         margin: 2em 0;
+    }
+    ul {
+        list-style: none;
     }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,36 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
-import { router } from './router'
+const app = createApp(App)
+
+import { createPinia } from 'pinia'
+app.use(createPinia())
+
+import 'bootstrap'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import 'bootstrap'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
-import BaseModal from '@/components/app/UI/BaseModal.vue'
-import BaseSpinner from '@/components/app/UI/BaseSpinner.vue'
-
 library.add(fas);
 library.add(far);
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(Toast, { showCloseButtonOnHover: true,})
 app.component('FontAwesome', FontAwesomeIcon)
+
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+app.use(Toast, { showCloseButtonOnHover: true,})
+
+import { plugin, defaultConfig } from '@formkit/vue'
+app.use(plugin, defaultConfig)
+
+import BaseModal from '@/components/app/UI/BaseModal.vue'
+import BaseSpinner from '@/components/app/UI/BaseSpinner.vue'
 app.component('BaseModal', BaseModal)
 app.component('BaseSpinner',BaseSpinner)
 
 import { useUserStore } from './components/modules/auth/stores/users';
 const userStore = useUserStore()
 
+import { router } from './router'
 userStore.initializeAuthListener().then(() => {
     app.use(router).mount('#app')
 })
