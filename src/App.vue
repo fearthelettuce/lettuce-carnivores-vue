69b7b8138c7d5f14f401002948a6b65ad921fbad
Brad Schickler <twigss@gmail.com>
2024-05-07 23:32:17 -0500

    Updated navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <TheHeader />
+  <TheNavbar />
   <RouterView />
 </template>
 
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
-import TheHeader from '@/components/app/header/TheHeader.vue'
+  import { RouterView } from 'vue-router'
+  import TheNavbar from '@/components/app/header/TheNavbar.vue'
 </script>
 
 <style lang="scss">
 @import '@/assets/scss/main';
+a {
+    text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
 </style>
