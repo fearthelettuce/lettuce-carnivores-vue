b949db12978af0cb29501a89aa924cebf022beb0
Brad Schickler <twigss@gmail.com>
2024-05-12 23:52:19 -0500

    Updated header styles

--- src/components/app/header/TheNavbar.vue
+++ src/components/app/header/TheNavbar.vue
@@ -9,7 +9,7 @@
             </div>
             <div class="flex flex-wrap items-center justify-between mx-auto p-4">
                 <router-link to="/" class="logo-link">
-                    <span class="logo self-center text-2xl font-semibold whitespace-nowrap dark:text-white" >Danger Lettuce</span>
+                    <span class="logo text-lime-400 self-center text-2xl font-semibold whitespace-nowrap" >Danger Lettuce</span>
                 </router-link>
                 <div class="flex md:order-2">
                     <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
@@ -18,15 +18,7 @@
                         </svg>
                         <span class="sr-only">Search</span>
                     </button>
-                    <div class="relative hidden md:block">
-                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                            </svg>
-                            <span class="sr-only">Search icon</span>
-                        </div>
-                        <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-lime-500 focus:border-lime-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-lime-500 dark:focus:border-lime-500" placeholder="Search...">
-                    </div>
+                    <SearchInput />
                     <button data-collapse-toggle="navbar-search" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -35,7 +27,7 @@
                     </button>
                 </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-                    <ul class="flex flex-col align-center p-4 mx-2 mb-0 md:p-0 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
+                    <ul class="flex flex-col align-center p-4 mx-2 mb-0 md:p-0 font-medium md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 ">
                         <li v-for="item of allowedNavLinks" :key="item.name">
                         <router-link
                             :to="item.path"
@@ -83,6 +75,7 @@ import { computed } from 'vue';
 import { navData } from '@/router/index'
 import { useUserStore } from '@/components/modules/auth/stores/users';
 import { useRoute } from 'vue-router';
+import SearchInput from './SearchInput.vue';
 
 const route = useRoute();
 const userStore = useUserStore()
