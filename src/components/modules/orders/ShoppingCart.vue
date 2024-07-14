30a00764bc0382f0d7de93b1fe905f6a2d3e85a7
fearthelettuce <twigss@gmail.com>
2024-07-14 13:41:10 -0500

    worked on cart

--- src/components/modules/orders/ShoppingCart.vue
+++ src/components/modules/orders/ShoppingCart.vue
@@ -1,51 +1,78 @@
 <template>
     <div class="container">
-        <div class="alert alert-warning">
+        <div class="alert alert-warning mb-5">
 
             The shopping cart is still under construction.  Please message @dangerlettuce on Instagram, Facebook, or eBay to purchase any of the plants listed here.
 
         </div>
         <div v-if="cart.cartItems.length === 0">
-            Cart is empty
+           <p>Cart is empty</p> 
         </div>
-        <div class="cart-grid">
-            <header class="cart-item-labels">
-                <div class="grid-col-2">Item</div>
-                <div class="text-center">Size</div>
-                <div class="text-center">Price</div>
-                <div>Quantity</div>
-            </header>
-            <div v-for="item in cart.cartItems" :key="item.sku" >
-                <div class="cart-item-container">
-                    
-                    <div class="cart-item-grid">
-                        <div class="grid-col-2"><router-link :to="`/plants/${encodeURIComponent(item.categoryId)}/${item.sku}`">{{`${item.name}${!item.isRepresentative ? ': specimen ' + item.sku : ''}`}}</router-link></div>
-                        <div class="text-center">{{ item.size }}</div>
-                        <div class="text-center">{{USDollar.format(item.price) }}</div>
-                        <div class="input-group">
-                            <span class="input-group-prepend">
-                                <button type="button" class="btn btn-outline-light btn-number" @click="decreaseQuantity(item)">
-                                    <FontAwesome class="text-light" icon="fa fa-minus"></FontAwesome>
-                                </button>
-                            </span>
-                            <input type="text" class="form-control input-number text-center" :value="item.quantity">
-                            <span class="input-group-append">
-                                <button type="button" class="btn btn-outline-light btn-number" :disabled="item.quantity >= item.maxQuantity" @click="increaseQuantity(item)">
-                                    <FontAwesome class="text-light" icon="fa fa-plus"></FontAwesome>
-                                </button>
-                            </span>
+
+        <div class="cart-grid" v-else>
+            <div class="cart-item-container ">
+
+                <div v-for="item in cart.cartItems" :key="item.sku" class="cart-item">
+                    <router-link :to="`/plants/${encodeURIComponent(item.categoryId)}/${item.sku}`">
+                        <div class="cart-item-photo">
+                            <img :src="getImageUrl(item.photo?.path)" :class="getImageUrl(item.photo?.path) == placeholderUrl ? 'placeholderImage': 'cardImage'" :alt="`An image of ${item.name}`" />
                         </div>
+                    </router-link>
+
+                        <div class="item-info">
+                                <div class="grid-col-2"><router-link :to="`/plants/${encodeURIComponent(item.categoryId)}/${item.sku}`">{{`${item.name}`}}</router-link></div>
+                                <div class="my-1">Size: {{ item.size }}</div>
+                                <div class="my-1" v-if="!item.isRepresentative">Specimen {{ item.sku }}</div>
+                                <div class="quantity-input mt-2 input-group input-group-sm">
+                                    <span class="input-group-prepend">
+                                        <button type="button" class="btn btn-outline-light btn-number btn-sm" @click="decreaseQuantity(item)">
+                                            <FontAwesome class="text-light" icon="fa fa-minus"></FontAwesome>
+                                        </button>
+                                    </span>
+                                    <input type="text" class="form-control input-number text-center btn-sm" :value="item.quantity">
+                                    <span class="input-group-append">
+                                        <button type="button" class="btn btn-outline-light btn-number btn-sm" :disabled="item.quantity >= item.maxQuantity" @click="increaseQuantity(item)">
+                                            <FontAwesome class="text-light" icon="fa fa-plus"></FontAwesome>
+                                        </button>
+                                    </span>
+                                </div>
+                            
+                        </div>
+
+                    <div class="item-subtotal align-content-center">
+                        <div class="text-center">{{USDollar.format(item.price * item.quantity) }}</div>
                     </div>
                 </div>
             </div>
-            <footer class="d-flex flex-row justify-content-center gap-5">
-                <h3>Subtotal</h3>
-                <h3>
-                    {{ USDollar.format(cartTotal)}}
-                </h3>
+            <footer>
+                <div class="subTotal-grid">
+                    
+                </div>
+                <div class="d-flex flex-row justify-content-end gap-5 m-3">
+                    <h3>Subtotal</h3>
+                    <h3>
+                        {{ USDollar.format(cartTotal)}}
+                    </h3>
+                    
+                </div>
+                <div class="d-flex flex-row justify-content-end gap-5 m-3">
+                    <h3>Shipping</h3>
+                    <h3>
+                        {{ USDollar.format(0)}}
+                    </h3>
+                    
+                </div>
+                <div class="d-flex flex-row justify-content-center">
+                    <button class="btn btn-primary" @click.prevent="checkout">Proceed to Checkout</button>
+                </div>
             </footer>
         </div>
     </div>
+
+
+
+
+
 </template>
 
 <script setup lang="ts">
@@ -53,6 +80,7 @@ import { storeToRefs } from 'pinia';
 import { useOrderStore } from '../products/stores/cart';
 import { computed, onMounted } from 'vue'
 import type { CartItem } from '@/types/Orders'
+import {getPhotoUrl, placeholderUrl} from '@/composables/usePhotoUtils'
 
 const { cart } = storeToRefs(useOrderStore())
 const { getCategoryBySku, addItemToCart, removeItemFromCart } = useOrderStore()
@@ -91,15 +119,53 @@ function deleteItem(item: CartItem) {
     removeItemFromCart(item, true)
 }
 
+function getImageUrl(path: string) {
+    return getPhotoUrl(path ?? null)
+}
+
+function checkout() {
+    console.log(cart.value.cartItems)
+}
 </script>
 
 <style scoped>
 
-.cart-grid {
-    display: grid;
-    grid-template-columns: repeat(1fr);
-    gap: 1rem;
+.cart-item {
+    display: flex;
+    flex-direction: row;
+    margin: 1rem .5rem
 }
+.cart-item-photo {
+    img {
+        width: 10rem;
+        height: 10rem;
+        object-fit: cover;
+    }
+    
+}
+.item-subtotal {
+    margin: 0 1rem 0 auto;
+}
+.item-footer {
+    display: flex;
+    flex-direction: row;
+    justify-content:space-between;
+    margin: 0 1rem;
+}
+.item-details {
+    padding: 1rem;
+    margin: 0 1rem;
+}
+.item-info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin: 1rem 2rem;
+}
+.quantity-input {
+    width: 6rem;
+}
+
 .cart-item-container {
     background:var(--bs-body-bg);
     filter: brightness(85%);
