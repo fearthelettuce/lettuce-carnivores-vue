0dcf560586bb869461e13507a870cd6a8410cbc3
fearthelettuce <twigss@gmail.com>
2024-07-31 00:02:11 -0500

    added fresh division message

--- src/components/modules/products/components/ProductDetails.vue
+++ src/components/modules/products/components/ProductDetails.vue
@@ -32,7 +32,9 @@
                         @click="setSelectedPlant(plant)"
                     >{{`Specimen ${plant.id} - ${plant.size}`}}</button>
                 </div>
-                
+                <div v-if="freshDivision?.isFreshDivision" class="mt-4">
+                    <p class="text-warning text-center">{{ freshDivision.message }}</p>
+                </div>
                 <div v-if="selectedPlant !== undefined" class="mt-4">
                     <h5 class="mb-3">{{plantTypeLabel}}</h5>
                     <small>{{ plantTypeDescription }}</small>
@@ -207,6 +209,37 @@ async function addToCart() {
         }
     }
 }
+
+const daysSinceDivision = computed(() => {
+    if(!selectedPlant.value || !selectedPlant.value?.propagationDate) {return undefined}
+    return Math.floor((Date.parse(new Date().toDateString()) - Date.parse(selectedPlant.value.propagationDate)) / 86400000);
+})
+const freshDivision = computed (() => {
+    if(daysSinceDivision.value === undefined || !plantCategory.value || !selectedPlant.value?.propagationDate) {return {isFreshDivision: false, message: ''}}
+    // const daysSinceDivision = Math.floor((Date.parse(new Date().toDateString()) - Date.parse(selectedPlant.value.propagationDate)) / 86400000);
+    // console.log(daysSinceDivision)
+    // console.log(daysSinceDivision < 45)
+    const formattedPropagationDate =  new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+        }).format(new Date(selectedPlant.value.propagationDate));
+    if(plantCategory.value.genus === 'Heliamphora') {
+        return {
+            isFreshDivision: daysSinceDivision.value < 45, 
+            message: `This is division was taken on ${formattedPropagationDate} and may not yet be rooted or established. 
+        We've had good success in shipping fresh divisions and our live arrival guarantee does apply. However, un-rooted divisions need extra attention 
+        and therefore are not recommended for beginners. \nFresh divisions may be shipped bare-root, depending on the plant, to ensure the plant arrives safely.`
+        }
+    }
+    if(plantCategory.value.genus === 'Nepenthes') {
+        return {
+            isFreshDivision: daysSinceDivision.value < 14, 
+            message: `This is a freshly taken cutting, taken on ${formattedPropagationDate}, and is not rooted. In our experience, Nepenthes cuttings have a 
+            good success rate, but it definitely is not 100%. Our live arrival guarantee does apply, however we make no guarantees that this cutting will root.`
+        }
+    }
+})
 </script>
 
 <style scoped>
