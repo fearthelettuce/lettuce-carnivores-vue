719aa532552503b4c576edb505898bcc2737e0fe
Brad Schickler <twigss@gmail.com>
2025-01-13 20:12:10 -0600

    sass import

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '@import "@/assets/scss/_variables.scss";'
+        additionalData: '@use "@/assets/scss/_variables.scss" as *;'
       }
     }
   },
