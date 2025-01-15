f6acb5881e265f10f54cdde8a0765cff908828d6
Brad Schickler <twigss@gmail.com>
2025-01-15 01:09:04 -0600

    work on header

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -2,21 +2,32 @@
   <header class="w-full z-40 fixed top-0 left-0 bg-background">
     <div class="container relative mx-auto min-h-20 flex gap-4 flex-row lg:grid lg:grid-cols-3 items-center">
       <div class="justify-start items-center gap-4 lg:flex hidden flex-row">
-        <NavigationMenu class="flex justify-start items-start">
+        <NavigationMenu class="flex justify-between items-start">
           <NavigationMenuList class="flex justify-start gap-4 flex-row">
-            <NavigationMenuItem v-for="item in allowedNavLinks" :key="item.name">
-              <NavigationMenuLink @click="collapseNavbar">
-                <Button variant="ghost">
 
-                  <router-link :to="item.path" class="nav-link text-light"
+              <NavigationMenuItem v-for="item in allowedNavLinks" :key="item.name">
+                <NavigationMenuLink @click="collapseNavbar">
+                  <Button variant="ghost">
+                    
+                    <router-link :to="item.path" class="nav-link text-light"
                     @click="$emit('collapseNavbar')">{{ item.label }}</router-link>
-                </Button>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
+                  </Button>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
           </NavigationMenuList>
+          
         </NavigationMenu>
       </div>
+      <Sheet>
+        <SheetTrigger >Open</SheetTrigger>
+        <SheetContent side="top">
+          <div v-for="item in allowedNavLinks" :key="item.name">
+            <router-link :to="item.path" class="nav-link text-light" @click="$emit('collapseNavbar')">{{ item.label }}</router-link>
+          </div>
+        </SheetContent>
+      </Sheet>
     </div>
+
   </header>
 </template>
 
@@ -30,6 +41,16 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from '@/components/ui/navigation-menu'
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
 import { Button } from '@/components/ui/button'
 import { computed } from 'vue';
 import { navData } from '@/router/index'
@@ -38,6 +59,7 @@ import { useOrderStore } from '@/stores/order';
 import { storeToRefs } from 'pinia'
 
 //https://www.twblocks.com/blocks/headers/header1
+//https://github.com/shadcn-ui/ui/issues/761
 
 const { cartItemCount } = storeToRefs(useOrderStore())
 
