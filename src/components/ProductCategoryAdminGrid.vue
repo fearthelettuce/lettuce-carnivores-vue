a84db0c8ebf7cdca62e29cb3b5e0af39f1aa79c4
Brad Schickler <twigss@gmail.com>
2025-03-26 20:54:00 -0500

    grid

--- src/components/ProductCategoryAdminGrid.vue
+++ src/components/ProductCategoryAdminGrid.vue
@@ -2,11 +2,12 @@
 <template>
   <BaseGrid 
     id="product-category-admin"
-    :columnDefs="productCategoryGrid"
+    :columnDefs
     :defaultColumnDefs
     :gridOptions
     :rowData="productCategories"
     :isLoading
+    ref="productCategoryGridRef"
   />
 </template>
 
@@ -17,17 +18,20 @@ import { onMounted } from 'vue';
 import { useProductStore } from '@/stores/productStore';
 import { storeToRefs } from 'pinia';
 import { productCategoryList, productStatusList, subCategories } from "@/constants/constants";
+import { useTemplateRef } from 'vue';
 
   const productStore = useProductStore()
   const { productCategories, isLoading, isSaving }  = storeToRefs(productStore)
   const { getProducts } = productStore
 
+  const productCategoryGrid = useTemplateRef('productCategoryGridRef')
+  
   onMounted(() => {
     getProducts()
   })
 
   const defaultColumnDefs: ColDef = {
-  
+    editable: true,
   }
   
   const gridOptions: GridOptions = {
@@ -37,20 +41,19 @@ import { productCategoryList, productStatusList, subCategories } from "@/constan
     },
   }
   
-  const productCategoryGrid: ColDef[] = [
+  const columnDefs: ColDef[] = [
     { field: 'id', headerName: 'ID', editable: true, width: 64 },
     {
       field: 'category',
-      editable: true,
       cellEditor: 'agSelectCellEditor',
       cellEditorParams: {
         values: productCategoryList
       },
+      valueFormatter: (params) => params.data.id === '' ? 'New' : params.data.id,
       width: 120
     },
     {
       field: 'subCategory',
-      editable: true,
       cellEditor: 'agSelectCellEditor',
       cellEditorParams: getSubcategoryParams,
       width: 120
@@ -59,15 +62,15 @@ import { productCategoryList, productStatusList, subCategories } from "@/constan
     { field: 'description',  editable: true },
     {
       field: 'status',
-      editable: true,
       cellEditor: 'agSelectCellEditor',
       cellEditorParams: {
       values: productStatusList
       }
     },
     // { field: 'tags',  cellEditor: 'agTextCellEditor' },
-    { field: 'createdDate', cellDataType: 'date', editable: true, cellEditor: 'agDateCellEditor', },
-    { field: 'dateUpdated', cellDataType: 'date', editable: true, cellEditor: 'agDateCellEditor', },
+    { field: 'createdDate', cellDataType: 'date', cellEditor: 'agDateCellEditor', },
+    { field: 'dateUpdated', cellDataType: 'date', cellEditor: 'agDateCellEditor', },
+    { field: 'saveStatus', cellDataType: 'text', editable: false }
   ]
 
   function getSubcategoryParams(params: ICellRendererParams): ISelectCellEditorParams {
